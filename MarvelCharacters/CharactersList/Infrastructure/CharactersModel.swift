a484b9e876280c91127ba18341583b6d2306cfec
chavesyuri-mb <79917740+chavesyuri-mb@users.noreply.github.com>
2022-09-10 12:11:14 -0400

    Add ChactersModel

---
+++ MarvelCharacters/CharactersList/Infrastructure/CharactersModel.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+// MARK: - Characters
+struct CharactersData: Decodable {
+    let data: CharactersDataModel
+}
+
+// MARK: - DataClass
+struct CharactersDataModel: Decodable {
+    let offset, limit, total, count: Int
+    let results: [CharactersResult]
+}
+
+// MARK: - Result
+struct CharactersResult: Decodable {
+    let id: Int
+    let name, resultDescription: String
+    let thumbnail: Thumbnail
+    let resourceURI: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case resultDescription = "description"
+        case thumbnail, resourceURI
+    }
+}
+
+// MARK: - Thumbnail
+struct Thumbnail: Decodable {
+    let path: String
+    let thumbnailExtension: String
+
+    enum CodingKeys: String, CodingKey {
+        case path
+        case thumbnailExtension = "extension"
+    }
+}
